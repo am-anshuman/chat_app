1491beeb2f271c1bac20a7a2f03eb4cee64d3253
am-anshuman <anshumansahu0310@gmail.com>
2023-07-16 15:53:47 +0530

    app completed

---
+++ lib/widgets/user_image_picker.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({
+    super.key,
+    required this.onPickImage,
+  });
+
+  final void Function(File pickedImage) onPickImage;
+
+  @override
+  State<UserImagePicker> createState() => UserImagePickerState();
+}
+
+class UserImagePickerState extends State<UserImagePicker> {
+  File? _pickeedImageFile;
+
+  void _pickImage() async {
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+
+    if (pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      _pickeedImageFile = File(pickedImage.path);
+    });
+
+    widget.onPickImage(_pickeedImageFile!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          foregroundImage:
+              _pickeedImageFile != null ? FileImage(_pickeedImageFile!) : null,
+        ),
+        TextButton.icon(
+          onPressed: _pickImage,
+          icon: const Icon(Icons.image),
+          label: Text(
+            'Add Image',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
